4579ca0ee28e4a890c07e129b47b50fd5d1f0a24
Vivianna Tilander <viva95@windowslive.com>
2020-03-10 18:06:34 +0200

    Tehtävä 7.2: routed anecdotes, step2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useParams
 } from "react-router-dom"
 
 const Menu = () => {
@@ -23,10 +24,27 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)
+      }
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(a => a.id === id)
+
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>Has {anecdote.votes} votes</p>
+      <p>For more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -135,6 +153,9 @@ const App = () => {
         <Menu />
 
         <Switch>
+          <Route path='/anecdotes/:id'>
+            <Anecdote anecdotes={anecdotes} />
+          </Route>
           <Route path='/about'>
             <About />
           </Route>
